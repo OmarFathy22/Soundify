f151504810d5904998eb2c6aa32418e332390127
Omar Fathy <91073589+OmarFathy22@users.noreply.github.com>
2024-03-14 11:04:09 +0200

    new

--- app/liked/page.tsx
+++ app/liked/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 import getLikedSongs from "@/actions/getLikedSongs";
-import Layout from '../../components/Layout'
+import Layout from "../../components/Layout";
 import AuthButtons from "@/components/HomeHeader/AuthButtons";
 import Title from "@/components/HomeHeader/Title";
 import ArrowRouter from "@/components/HomeHeader/ArrowsRouting";
@@ -16,33 +16,28 @@ const Liked = async () => {
   const songs = await getLikedSongs();
 
   return (
-    
     <Layout>
-    <div className="bg-gradient-to-b from-emerald-700 p-5 rounded-t-lg ">
-      <div className="flex justify-between">
-        <ArrowRouter />
-        <AuthButtons />
-      </div>
-
-      <div className="flex xs:flex-col items-center gap-3 mt-10">
-        <div className="h-[84px] w-[84px]">
-          <Image src="/liked.png" alt="liked" width={200} height={200}/>
+      <div className="bg-gradient-to-b from-emerald-700 p-5 rounded-t-lg ">
+        <div className="flex justify-between">
+          <ArrowRouter />
+          <AuthButtons />
         </div>
-        <div>
-          <p className="text-neutral-400 xs:text-center">playlist</p>
-          <h1 className="text-4xl font-bold xs:text-center">Favorite Songs</h1>
+
+        <div className="flex xs:flex-col items-center gap-3 mt-10">
+          <div className="h-[84px] w-[84px]">
+            <Image src="/liked.png" alt="liked" width={200} height={200} />
+          </div>
+          <div>
+            <p className="text-neutral-400 xs:text-center">playlist</p>
+            <h1 className="text-4xl font-bold xs:text-center">
+              Favorite Songs
+            </h1>
+          </div>
         </div>
-        
       </div>
-
-
-
-      
-    </div>
-        <LikedSongsContent  songs={songs}/>
-      
-  </Layout>
+      <LikedSongsContent songs={songs} />
+    </Layout>
   );
-}
+};
 
 export default Liked;
